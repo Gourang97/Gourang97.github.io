02d9798e12764808439cca8b4a700fc8d2cfc896
Gourang_git <gourang.patel97@gmail.com>
2021-05-27 02:58:12 -0400

    fix:minor bug fix in index.html

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78b4a611023cdf9c1f4dc19098d25f68.js"
+  "/precache-manifest.56e8946d3c09fafc2230494f15c45e3e.js"
 );
 
 self.addEventListener('message', (event) => {
